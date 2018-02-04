83799f5a7e60587d9d16d240dadf398734e828a4
QuangPro <boybuj94>
2018-02-04 13:09:18 +0700

    update menu and css

--- view/modules/menu-vi.php
+++ view/modules/menu-vi.php
@@ -1,14 +1,38 @@
-<div class="menu">
-  <nav>
+<div>
+  <nav class="menu">
+    <div class="top-menu-timkiem">
+        <form method='get' action='index.php'>
+              <input type='submit' name='seach' value=''/>
+              <input type='text' name= 'text_seach' placeholder = '&nbsp;&nbsp;Tìm kiếm sản phẩm !'/>        
+        </form>
+      </div>
   <ul id="nav">
-  	<a href="index.php" title="Trang chủ" style="float: left; padding: 13px;"><img src="public/img/icon-home.png" width="20px" style="padding-right:10px;"/></a>
-  	<li><a href="index.php?manage=Introduce" title="Giới thiệu"><i class="fa fa-envelope site-nav--icon"></i>Giới thiệu</a></li>
-    <li><a href="products/san-pham.html" title="Thiết bị"><i class="fa fa-envelope site-nav--icon"></i>Sản phẩm</a></li>
-    <li><a href="download.html" title="Download phần mềm"><i class="fa fa-envelope site-nav--icon"></i>Download</a></li>
-    <li><a href="dich-vu.html" title="Nội thất ý tế"><i class="fa fa-envelope site-nav--icon"></i>Nội thất ý tế</a></li>
-    <li><a href="dich-vu.html" title="Công trình"><i class="fa fa-envelope site-nav--icon"></i>Công trình</a></li>
-    <li><a href="dich-vu.html" title="Dịch vụ"><i class="fa fa-envelope site-nav--icon"></i>Dịch vụ</a></li>
-    <li><a href="contact.html" title="Liên hệ"><i class="fa fa-envelope site-nav--icon"></i>Liên hệ</a></li>
+  	<a class="icon-home" href="index.php" title="Trang chủ" style=""><img src="public/img/icon-home.png" width="20px" style="padding-right:10px;"/></a>
+  	<li><a href="index.php?manage=Introduce" title="Giới thiệu">Giới thiệu</a></li>
+    <li><a href="products/san-pham.html" title="Thiết bị">Sản phẩm</a></li>
+    <li><a href="download.html" title="Download phần mềm">Download</a></li>
+    <li><a href="dich-vu.html" title="Nội thất ý tế">Nội thất ý tế</a></li>
+    <li><a href="dich-vu.html" title="Công trình">Công trình</a></li>
+    <li><a href="dich-vu.html" title="Dịch vụ">Dịch vụ</a></li>
+    <li><a href="contact.html" title="Liên hệ">Liên hệ</a></li>
   </ul>
+  <a href='#' id='pull'></a>   
 </nav>
-</div>	
+</div>
+<script> $(function() {
+  var pull        = $('#pull');
+      menu        = $('nav ul');
+      menuHeight  = menu.height();
+
+  $(pull).on('click', function(e) {
+      e.preventDefault();
+      menu.slideToggle();
+  });
+});
+$(window).resize(function(){
+  var w = $(window).width();
+  if(w > 780 && menu.is(':hidden')) {
+      menu.removeAttr('style');
+  }
+});
+</script>
